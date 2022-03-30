559bbc133f93e8e32b2af1f795cb2cbb72633f75
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-03-30 20:15:36 +0600

    Fast Commit 30/03/2022

---
+++ Addition (  যোগ )/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Addition (+) </title>
+</head>
+<body>
+    
+
+<?php
+
+    $a = 10;
+    $b = 20;
+
+    // Above দুই a and $ b are the two variables whose values are given.
+
+
+    echo $a + $b ;
+ 
+    //The results are being printed or seen by echoing here.
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+</body>
+</html>
---
+++ Modulus (ভাগ শেষ )/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modulus (ভাগ শেষ) </title>
+</head>
+<body>
+    
+
+<?php
+
+    $a = 13;
+    $b = 3;
+
+    // Above দুই a and $ b are the two variables whose values are given.
+
+
+    echo $a + $b ;
+ 
+    //The results are being printed or seen by echoing here.
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+</body>
+</html>
